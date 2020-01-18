3bff4ccf2b6609ccea2d206b81f58d269ba086d7
MINILOBIN <super6649@gmail.com>
2020-01-18 22:39:56 +0900

    fourth

--- GUILD.py
+++ GUILD.py
@@ -24,7 +24,7 @@ async def on_message(message):
         boss = 0
         while 1:
             now = datetime.now()
-            if now.minute == 30:
+            if now.minute == 50:
                 if now.hour == 11 and flag1 == 0:
                     await channel.send('모두 플래그 하러 갑시다!!')
                     flag1 = 1
@@ -37,14 +37,14 @@ async def on_message(message):
                     flag2 = 1
                     flag3 = 0
                     boss = 0
-                if now.hour == 22 and flag3 == 0:
+                if now.hour == 20 and flag3 == 0:
                     await channel.send('모두 플래그 하러 갑시다!!')
                     flag1 = 0
                     flag2 = 0
                     flag3 = 1
                     boss = 0
-            if now.minute == 31:
-                if now.hour == 22 and boss == 0:
+            if now.minute == 30:
+                if now.hour == 23 and boss == 0:
                     await channel.send('다들 오늘 길보 빼먹지 않고 돌았죠?! :)')
                     boss = 1
                     flag1 = 0
